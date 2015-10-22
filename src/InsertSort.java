632fd25ab218eeec3e741f40407fd6cec3044eb7
lee <showthesunli@gmail.com>
2015-10-22 23:28:32 +0800

    算法导论，代码实现

---
+++ src/InsertSort.java
@@ -0,0 +1,26 @@
+/**
+ * Created by showt on 2015/10/22.
+ */
+public class InsertSort {
+    public int[] solutaion(int [] A){
+        for (int i=1;i<A.length;i++){
+            int key = A[i];
+            int j = i - 1;
+            while (j>=0 && key>A[j]){
+               A[j+1] = A[j];
+                A[j] = key;
+                --j;
+            }
+        }
+       return A;
+    }
+
+    public static void main(String [] args){
+        int [] A = new int[] {5,2,4,6,1,3};
+        new InsertSort().solutaion(A);
+        for (int i=0;i<A.length;i++) {
+            System.out.println(A[i]);
+        }
+
+    }
+}

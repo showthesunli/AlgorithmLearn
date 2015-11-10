bfecebd8b8099f0a8362e4394211a4e295f76d9c
lee <showthesunli@gmail.com>
2015-11-10 23:46:33 +0800

    调整工程结构

---
+++ src/firsttime/HeapMaximum.java
@@ -0,0 +1,26 @@
+package firsttime;
+
+/**
+ * 优先级队列
+ * Created by showt on 2015/11/10.
+ */
+public class HeapMaximum {
+    /**
+     * 返回堆中最大元素
+     * @param A 堆数组
+     * @return
+     */
+    public static int heapMaxiMum(int []A){
+        return A[0];
+    }
+
+    /**
+     * 返回并删除最大堆元素
+     * @param A 堆数组
+     * @return
+     */
+    public static int heapExtractMax(int [] A){
+
+        return 0;
+    }
+}
